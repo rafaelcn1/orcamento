21f7ffc97cda23af60884a8642484c0cad0fc6bf
rafaelcn1 <rafaelcn1@hotmail.com>
2023-01-24 15:00:04 -0300

    envio do projeto maven

---
+++ src/test/java/br/com/orcamento/OrcamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.orcamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrcamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
